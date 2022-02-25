a34d3f37e00cbecc149f245b1e7aa7859d7b3152
lilykr <lisa-lou.k@hotmail.fr>
2022-02-25 10:56:32 +0100

    first api call homepage

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text } from 'react-native';
 
 import { View } from '../components/Themed';
+import useReduxAppState from '../hooks/useAppState';
+import useReduxUserState from '../hooks/useUserState';
 
 export default function HomeScreen() {
+  const { user } = useReduxUserState();
   return (
     <View style={styles.container}>
-      <Text>home screen</Text>
+      <Text style={styles.textHeader}>Welcome {user.first_name} 👋🏻</Text>
     </View>
   );
 }
@@ -13,5 +16,10 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  textHeader: {
+    fontFamily: 'Avenir-Heavy',
+    fontSize: 20,
+    marginLeft: 15,
   },
 });
